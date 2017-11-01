a38d512f5a1fd5b17a8a0be8e7f5a1b3b6fbdd92
Waqar-107 <waqar.hassan866@gmail.com>
2017-11-01 16:01:59 +0700

    without php

---
+++ login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <title>login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="css/login_cs.css" rel="stylesheet">
+</head>
+
+
+<body>
+
+<div class="container">
+
+    <!--sign in panel-->
+    <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+
+            <div class="panel panel-default">
+                <h3 class="text-center login-title" style="color:white;padding-bottom: 10px">Sign In</h3>
+                <div class="panel-body">
+                    <form action="">
+                        <div class="form-group">
+                            <input type="email" name="" class="form-control" placeholder="email">
+                        </div>
+
+                        <div class="form-group" style="margin-top: 10px">
+                            <input type="password" name="" class="form-control" placeholder="password">
+                        </div>
+
+                        <div class="form-group" style="margin-top: 50px">
+                            <input type="submit" name="" class="btn btn-success btn-lg btn-block" value="login">
+                        </div>
+
+                        <!--forget password and sign-up-->
+                        <a class="su" href="#">sign up</a>
+
+                        <a class="fp" href="#" id="forgot_password">forgot password</a>
+                        <script>
+                            document.getElementById("forgot_password").onclick=function () {
+                                alert("password has been sent to your mail!")
+                            }
+                        </script>
+                        <!--forget password and sign-up-->
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--sign in panel-->
+</div>
+
+</body>
+
+
+</html>
