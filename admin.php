482483860a5b6610092ba50cca129172f90e5516
Waqar-107 <waqar.hassan866@gmail.com>
2017-11-01 20:53:35 +0700

    added php code to take info

--- admin.php
+++ admin.php
@@ -1,3 +1,44 @@
+<?php
+
+    if(isset($_POST['submit']))
+    {
+        $id=$_POST['id'];
+        $pass=$_POST['pass'];
+
+        if(empty($id) || empty($pass))
+        {
+            if (empty($id) && empty($pass))
+            {
+                echo '<script language="javascript">';
+                echo 'alert("please fill all of the informations!!!")';
+                echo '</script>';
+            }
+
+
+            else if(empty($id) )
+            {
+                echo '<script language="javascript">';
+                echo 'alert("enter user id")';
+                echo '</script>';
+            }
+
+            else if(empty($pass))
+            {
+                echo '<script language="javascript">';
+                echo 'alert("enter password")';
+                echo '</script>';
+            }
+        }
+
+        else
+        {
+            //redirect to home
+        }
+    }
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -23,17 +64,17 @@
             <div class="panel panel-default">
                 <h3 class="text-center login-title" style="color:white;padding-bottom: 10px">Sign In</h3>
                 <div class="panel-body">
-                    <form action="">
+                    <form action="" method="post">
                         <div class="form-group">
-                            <input type="text" name="" class="form-control" placeholder="user_id">
+                            <input type="text" name="id" id="id" class="form-control" placeholder="user_id">
                         </div>
 
                         <div class="form-group" style="margin-top: 10px">
-                            <input type="password" name="" class="form-control" placeholder="password">
+                            <input type="password" name="pass" id="pass" class="form-control" placeholder="password">
                         </div>
 
                         <div class="form-group" style="margin-top: 50px">
-                            <input type="submit" name="" class="btn btn-success btn-lg btn-block" value="login">
+                            <input type="submit" name="submit" id="submit" class="btn btn-success btn-lg btn-block" value="login">
                         </div>
 
                         <!--forget password-->
