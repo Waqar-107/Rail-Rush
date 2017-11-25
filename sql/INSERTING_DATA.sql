ecbc3766848fb60f205d48376750614a5d34c29f
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2017-11-25 19:34:55 +0700

    Update INSERTING_DATA.sql

--- sql/INSERTING_DATA.sql
+++ sql/INSERTING_DATA.sql
@@ -1,11 +1,11 @@
 /*------------------------------------------------------------------------------------------------------------------*/
 /*INSERT ADMIN DATAS*/
 
-INSERT INTO ADMIN VALUES(1,'Waqar','Hassan Khan','waqar.hassan866@gmail.com','201505107','phoenix');
-INSERT INTO ADMIN VALUES(2,'Tom','Marvolo Riddle','1505107.whk@ugrad.cse.buet.ac.bd','201505101','crucio');
-INSERT INTO ADMIN VALUES(3,'Severus','Snape','anfuad@yahoo.com','201505102','sectumsempra');
-INSERT INTO ADMIN VALUES(4,'Ron','Weasly','kowshid@gmail.com','201505103','spider');
-INSERT INTO ADMIN VALUES(5,'Draco','Malfoy','farhan.tanvir.utshaw@gmail.com','201505104','basilisk');
+INSERT INTO ADMIN VALUES(1,1,'Waqar','Hassan Khan','waqar.hassan866@gmail.com','201505107','phoenix');
+INSERT INTO ADMIN VALUES(2,2,'Tom','Marvolo Riddle','1505107.whk@ugrad.cse.buet.ac.bd','201505101','crucio');
+INSERT INTO ADMIN VALUES(3,3,'Severus','Snape','anfuad@yahoo.com','201505102','sectumsempra');
+INSERT INTO ADMIN VALUES(4,4,'Ron','Weasly','kowshid@gmail.com','201505103','spider');
+INSERT INTO ADMIN VALUES(5,5,'Draco','Malfoy','farhan.tanvir.utshaw@gmail.com','201505104','basilisk');
 
 
 /*------------------------------------------------------------------------------------------------------------------*/
