8c9a53f1b902bd63930d1e225a710fc4f1e79838
Waqar-107 <waqar.hassan866@gmail.com>
2017-12-28 13:43:59 +0700

    added charts of tender

--- tender.php
+++ tender.php
@@ -1,6 +1,8 @@
 <?PHP
 
     session_start();
+    echo '<script src="js/plotly-latest.min.js" type="text/javascript"></script>';
+
     if(empty($_SESSION['user_in']))
     {
         header('location: base.php');
@@ -11,6 +13,17 @@
     {
         header('Location: base.php');
     }
+
+    //---------------------------------------------------------------connect to the database
+    //create connection
+    $conn = oci_connect('ANONYMOUS', '1505107', 'localhost/orcl');
+
+    //check connection
+    if(!$conn)
+    {
+        echo 'connection error';
+    }
+    //---------------------------------------------------------------connect to the database
 
     //---------------------------------------------------------------add new entry
     echo '<div class="container-fluid" style="margin-top: 100px">
@@ -24,10 +37,31 @@
                       </div>';
     //---------------------------------------------------------------add new entry
 
-    //---------------------------------------------------------------show graph 1
-    
-    //---------------------------------------------------------------show graph 1
+    //---------------------------------------------------------------data of graph 2
+    $sql="SELECT TO_CHAR(TDATE,'YYYY-MM-DD'),SUM(COSTING)
+          FROM TENDER
+          GROUP BY TDATE
+          ORDER BY TDATE";
 
+    $result=oci_parse($conn,$sql);
+    oci_execute($result);
+    $mtime=array();$cost=array();
+    while($row=oci_fetch_assoc($result))
+    {
+        array_push($mtime,$row['TO_CHAR(TDATE,\'YYYY-MM-DD\')']);array_push($cost,$row['SUM(COSTING)']);
+    }
+    //---------------------------------------------------------------data of graph 2
+
+    //---------------------------------------------------------------data of graph 2
+    $sql="SELECT COMPANY, SUM(COSTING) FROM TENDER
+          GROUP BY COMPANY";
+    $result=oci_parse($conn,$sql);oci_execute($result);
+    $com=array();$comPrice=array();
+    while($row=oci_fetch_assoc($result))
+    {
+        array_push($com,$row['COMPANY']);array_push($comPrice,$row['SUM(COSTING)']);
+    }
+    //---------------------------------------------------------------data of graph 2
 
 ?>
 
@@ -40,6 +74,7 @@
     <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"/>
     <link href="css/tender.css" rel="stylesheet"/>
     <script src="js/showDate.js" type="text/javascript"></script>
+
 </head>
 <body>
 
@@ -55,6 +90,60 @@
         </script>
     </nav>
 </div>
+
+<div class="container-fluid" style="background-color: whitesmoke">
+    <div class="container" style="margin-top: 150px;width: 95%" id="timeCost"></div>
+</div>
+<script type="text/javascript">
+    var x=<?php echo json_encode($mtime);?>;
+    var y=<?php echo json_encode($cost);?>;
+    var data=[{
+        x:x,
+        y:y,
+        name:'time-cost',
+        type:'scatter'}
+    ];
+
+    var layout={
+        title:'Date-Cost',
+        xaxis:{
+            title:'Date→',
+
+        },
+
+        yaxis:{
+            title:'Cost→'
+        }
+    };
+
+    Plotly.newPlot('timeCost',data,layout);
+</script>
+
+<div class="container-fluid" style="background-color: whitesmoke">
+    <div class="container" style="margin-top: 150px ; width: 95%" id="comCost"></div>
+</div>
+<script>
+    var x=<?php echo json_encode($com);?>;
+    var y=<?php echo json_encode($comPrice);?>;
+    var trace={
+        x:x,y:y,type:'bar'
+    };
+
+    var layout={
+        title:'Company-Cost',
+        xaxis:{
+            title:'Company→',
+
+        },
+
+        yaxis:{
+            title:'Cost→'
+        }
+    };
+
+    var data=[trace];
+    Plotly.newPlot('comCost',data,layout);
+</script>
 
 </body>
 </html>
